f4c440ff1187c39f06dd3132c9d7277ca20166fd
alexanderkalyupanov <138789735+alexanderkalyupanov@users.noreply.github.com>
2024-06-04 18:57:35 +0300

    create object data in js

--- js/player.js
+++ js/player.js
@@ -1,1 +1,56 @@
-const 
+const data = {
+    playlists: [
+        {
+            playlistInfo: {
+                id: "1-0",
+                title: "Hip-Hop Hits",
+                coverImgUrl: "img/cardImage/image1.jpeg",
+                trackCount: 4,
+            },
+            tracks: [
+                {
+                    trackId: "1",
+                    artistName: "Eminem",
+                    traskTitle: "Rap God",
+                    isHot: true,
+                    trackFileUrl: "audio/Eminem - Rap God.mp3",
+                    trackCoverImgUrl: "img/cardImage/trackList/track1.jpeg",
+                },
+                {
+                    trackId: "2",
+                    artistName: "50 cent",
+                    traskTitle: "In da Club",
+                    isHot: false,
+                    trackFileUrl: "audio/50cent%20-%20In%20da%20club.mp3",
+                    trackCoverImgUrl: "img/cardImage/trackList/track2.jpeg",
+                }
+            ]
+        },
+        {
+            playlistInfo: {
+                id: "1-1",
+                title: "Rap Hits 1990s",
+                coverImgUrl: "img/cardImage/image2.jpeg",
+                trackCount: 4,
+            },
+            tracks: [
+                {
+                    trackId: "5",
+                    artistName: "Public Enemy",
+                    traskTitle: "Fight The Power",
+                    isHot: true,
+                    trackFileUrl: "audio/Public%20Enemy%20-%20Fight%20The%20Power.mp3",
+                    trackCoverImgUrl: "img/cardImage/trackList/track5.jpeg",
+                },
+                {
+                    trackId: "6",
+                    artistName: "Vanila Ice",
+                    traskTitle: "Ice Ice Baby",
+                    isHot: false,
+                    trackFileUrl: "audio/Vanila%20Ice%20-%20Ice%20Baby.mp3",
+                    trackCoverImgUrl: "img/cardImage/trackList/track6.jpeg",
+                },
+            ]
+        },
+    ]
+}
