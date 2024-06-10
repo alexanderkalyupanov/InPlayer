1cf8b4e9eb97a50bd1eae8c9578abe34afcdc40b
alexanderkalyupanov <138789735+alexanderkalyupanov@users.noreply.github.com>
2024-06-10 11:05:47 +0300

    add function in player.js
    AddTrackPanel(), List(), Track(), TrackDetails(), TrackTopLine(), ButtonsEditDelete()

--- js/player.js
+++ js/player.js
@@ -165,7 +165,7 @@ function Playlists() {
     container.classList.add("playlists");
 
     for (let i = 0; i < data.playlists.length; i++) {
-        container.append(Playlist([data.playlists[i]]))
+        container.append(Playlist(data.playlists[i]))
     }
 
     return container;
@@ -230,8 +230,158 @@ function PlaylistInfo(playlistInfo) {
 }
 
 function Tracklist(tracks) {
+    const container = document.createElement("div")
+    container.classList.add("tracklist")
+
+    container.append(
+        AddTrackPanel(),
+        List(tracks)
+    )
+
+    return container;
+}
+
+// AddTrackPanel
+
+function AddTrackPanel() {
+    const container = document.createElement("div");
+    container.classList.add("add-track-panel");
+
+    const title = document.createElement("h3");
+    title.innerText = "Tracks";
+    title.classList.add("tracklist-title");
+
+    const addTrackButton = document.createElement("button");
+    addTrackButton.classList.add("button-add-track")
+
+    const addTrackButtonImg = document.createElement("img");
+    addTrackButtonImg.src = "img/icons/add.svg"
+
+    addTrackButton.append(addTrackButtonImg, "Add Track")
+    container.append(title, addTrackButton)
+
+    return container
+}
+
+// List
+
+function List(tracks) {
+    const container = document.createElement("ul");
+    container.classList.add("list");
+
+    for (let i = 0; i < tracks.length; i++) {
+        container.append(Track(tracks[i]))
+    }
+
+    return container
 
 }
+
+// Track
+
+{/* <img class="track-cover-image" src="img/cardImage/trackList/track1.jpeg"
+alt="track-cover"> */}
+
+function Track(track) {
+    const container = document.createElement("li");
+    container.classList.add("track-element");
+
+    const trackCoverImg = document.createElement("img");
+    trackCoverImg.classList.add("track-cover-image");
+    trackCoverImg.src = track.trackCoverImgUrl;
+
+    container.append(
+        trackCoverImg,
+        TrackDetails(track)
+    )
+
+    return container
+
+}
+
+// TrackDetails
+
+function TrackDetails(track) {
+    const container = document.createElement("div");
+    container.classList.add("track-details");
+
+    const audio = document.createElement("audio");
+    audio.src = track.trackFileUrl;
+    audio.controls = true;
+
+    container.append(
+        TrackTopLine(track),
+        audio
+    )
+
+    return container
+
+}
+
+// TrackTopLine
+
+function TrackTopLine(track) {
+    const container = document.createElement("div");
+    container.classList.add("track-top-line");
+
+    if (track.isHot) {
+        const trackHotImg = document.createElement("img");
+        trackHotImg.classList.add("track-status");
+        trackHotImg.src = "img/icons/hot.svg"
+        container.append(trackHotImg)
+    }
+    const trackInfo = document.createElement("div")
+    trackInfo.classList.add("track-info")
+
+    const trackName = document.createElement("div");
+    trackName.classList.add("track-name")
+    trackName.innerText = track.artistName + "-" + track.trackTitle;
+    trackInfo.append(
+        trackName, 
+        ButtonsEditDelete()
+    )
+
+    container.append(trackInfo)
+
+    return container
+
+}
+
+//         ButtonsEditDelete()
+
+function ButtonsEditDelete() {
+    const buttonsContainer = document.createElement("div");
+    buttonsContainer.classList.add("buttons-container");
+
+    const editBtn = document.createElement("button")
+    const editBtnImg = document.createElement("img");
+    editBtnImg.src = "img/icons/edit.svg"
+    editBtn.append(editBtnImg)
+
+    const delBtn = document.createElement("button");
+    const delBtnImg = document.createElement("img");
+    delBtnImg.src = "img/icons/basket.svg";
+    delBtn.append(delBtnImg)
+
+    buttonsContainer.append(editBtn, delBtn)
+    return buttonsContainer
+}
+
+
+
+// createElement - функция для создания элементов на странице
+
+// function createElement(typeofElement, param) {
+//     const container = document.createElement(typeofElement);
+//     if (typeof param === "string") {
+//         container.classList.add(param)
+//     } else if (typeof param === "object") {
+//         for (const key in param) {
+
+//         }
+//     }
+//     return container
+// }
 
 const root = document.getElementById("root");
 
